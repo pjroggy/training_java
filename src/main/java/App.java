4fe5dacd2dd47364a8e9d0326ab65c2865925aa5
ChrisCassag <christophe.cassagnabere@evooq.ch>
2022-09-26 22:54:32 +0200

    Master class 3: genericite

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,10 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Battlefield!");
 
-		simpleCombat();
-		combatWithViking();
+//		simpleCombat();
+//		combatWithViking();
+		combatSwordsmanWithViking();
+		combatWithKnight();
 	}
 
 	public static void simpleCombat() {
@@ -13,7 +15,7 @@ public class App {
 		Swordsman swordsman1 = new Swordsman();
 		Swordsman swordsman2 = new Swordsman();
 
-		swordsman1.attack2(swordsman2);
+		swordsman1.attack(swordsman2);
 
 		System.out.println(swordsman1.hitPoints()); // should equal 5!
 		System.out.println(swordsman2.hitPoints()); // should equal 0! XX
@@ -24,9 +26,32 @@ public class App {
 		Viking viking1 = new Viking();
 		Viking viking2 = new Viking();
 
-		viking1.attack2(viking2);
+		viking1.attack(viking2);
 
 		System.out.println(viking1.hitPoints()); // should equal 6!
 		System.out.println(viking2.hitPoints()); // should equal 0! XX
+	}
+
+	public static void combatSwordsmanWithViking() {
+		Swordsman swordsman1 = new Swordsman();
+		Viking viking1 = new Viking();
+
+		swordsman1.attack(viking1);
+
+		System.out.println(swordsman1.hitPoints()); // should equal 0! XX
+		System.out.println(viking1.hitPoints()); // should equal 35!
+	}
+
+	// TODO implementer la classe Knight
+	public static void combatWithKnight() {
+		// A Knight has 100 hit points and use a 1 hand sword that does 5 dmg
+		// A knight wears an armor. an armor absorbs 2 hit points for each strike
+		Swordsman swordsman1 = new Swordsman();
+		Knight knight1 = new Knight();
+
+		swordsman1.attack(knight1);
+
+		System.out.println(swordsman1.hitPoints()); // should equal 0! XX
+		System.out.println(knight1.hitPoints()); // should equal 40!
 	}
 }
--- src/main/java/Fighter.java
+++ src/main/java/Fighter.java
@@ -1,53 +1,15 @@
 
-public class Fighter {
-	
-	private int hitpoints;
-	private int hitpointss;
-	
-		public Fighter() {
-			this.hitpoints = 100;
-			this.hitpointss = 120;
-		}
-		
-		public void attack1(Fighter other) {
-			Fighter swordsman = this;
-			Fighter viking = other;
-			
-			while (swordsman.hitpoints > 0 && viking.hitpointss >0) {
-				viking.hitpointss -= 5;
-			}
-			if(viking.hitpointss > 0) {
-				swordsman.hitpoints -=6;
-			}
-		}
-		
-		public void attack2(Fighter other) {
-			Fighter swordsman = this;
-			Fighter viking = other;
-			
-			while (swordsman.hitpoints > 0) {
-				viking.hitpointss -=5;
-				swordsman.hitpoints -=6;
-				
-				Fighter fighterTemp = swordsman;
-				swordsman = viking;
-				viking = fighterTemp;
-			}
-		}
-		
-		public void attack3(Fighter other) {
-			if(this.hitpoints >0 && this.hitpointss >0) {
-				other.hitpoints -= 6;
-				other.hitpointss -= 5;
-				
-				other.attack3(this);
-			}
-				
-	}
-		public int hitpoints;
-		public int hitpointss;	
-			
-		return this.hitpoints;
-		return this.hitpointss;
-	}	
+public abstract class Fighter {
+
+    protected int hitPoints;
+
+    public Fighter(int hitPoints){
+        this.hitPoints = hitPoints;
+    }
+
+    public int hitPoints() {
+        return Math.max(this.hitPoints, 0);
+    }
+
+    public abstract void attack(Fighter other);
 }		
